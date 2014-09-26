bdab0b6bd8e37fddcdf237408b81cba1e74659cb
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-09-26 15:42:54 -0400

    test- update tests

--- UnitTests/Tests/NSNumber+FISMathSpec.m
+++ UnitTests/Tests/NSNumber+FISMathSpec.m
@@ -13,26 +13,70 @@
 SpecBegin(NSNumberFISMath)
 
 describe(@"NSNumber+FISMath", ^{
+    __block NSNumber *x;
+    __block NSNumber *y;
     
     beforeAll(^{
-
+        x = [NSNumber numberWithInteger:14];
+        y = [NSNumber numberWithInteger:7];
     });
     
-    beforeEach(^{
-
+    it(@"should respond to add:",^{
+        expect(x).to.respondTo(@selector(add:));
     });
     
-    it(@"", ^{
-
-    });  
-    
-    afterEach(^{
-
+    it(@"should respond to subtract:",^{
+        expect(x).to.respondTo(@selector(subtract:));
     });
     
-    afterAll(^{
-
+    it(@"should respond to multiplyBy:",^{
+        expect(x).to.respondTo(@selector(multiplyBy:));
     });
+    
+    it(@"should respond to divideBy:",^{
+        expect(x).to.respondTo(@selector(divideBy:));
+    });
+    
+    describe(@"add:", ^{
+        it(@"should return an NSNumber", ^{
+            expect([x add:y]).to.beKindOf([NSNumber class]);
+        });
+        
+        it(@"should return the sum of two NSNumbers",^{
+            expect([x add:y]).to.equal(@21);
+        });
+    });
+    
+    describe(@"subtract:", ^{
+        it(@"should return an NSNumber",^{
+            expect([x subtract:y]).to.beKindOf([NSNumber class]);
+        });
+        
+        it(@"should return the difference between two NSNumbers",^{
+            expect([x subtract:y]).to.equal(@7);
+        });
+    });
+    
+    describe(@"multiply:", ^{
+        it(@"should return an NSNumber",^{
+            expect([x multiplyBy:y]).to.beKindOf([NSNumber class]);
+        });
+        
+        it(@"should return the product of two NSNumbers",^{
+            expect([x multiplyBy:y]).to.equal(@98);
+        });
+    });
+    
+    describe(@"divide:", ^{
+        it(@"should return an NSNumber",^{
+            expect([x multiplyBy:y]).to.beKindOf([NSNumber class]);
+        });
+        
+        it(@"should return the quotient of two NSNumbers",^{
+            expect([x divideBy:y]).to.equal(2);
+        });
+    });
+    
 });
 
 SpecEnd
