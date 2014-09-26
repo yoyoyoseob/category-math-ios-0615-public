0aee8af1637c819cbed1350d7f5aa7785db1142a
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-09-26 11:38:16 -0400

    add NSNumberFISMathSpec

---
+++ UnitTests/Tests/NSNumber+FISMathSpec.m
@@ -0,0 +1,38 @@
+//
+//  NSNumber+FISMathSpec.m
+//  CategoryMath
+//
+//  Created by Chris Gonzales on 9/26/14.
+//  Copyright 2014 The Flatiron School. All rights reserved.
+//
+
+#import "Specta.h"
+#import "NSNumber+FISMath.h"
+
+
+SpecBegin(NSNumberFISMath)
+
+describe(@"NSNumber+FISMath", ^{
+    
+    beforeAll(^{
+
+    });
+    
+    beforeEach(^{
+
+    });
+    
+    it(@"", ^{
+
+    });  
+    
+    afterEach(^{
+
+    });
+    
+    afterAll(^{
+
+    });
+});
+
+SpecEnd
