89b08ca71ece60277b84b36479c7e7b123b259b5
Chris Gonzales <gonzales.chris.a@gmail.com>
2014-09-26 16:28:26 -0400

    update testrunner for xcode 6

--- test_runner.sh
+++ test_runner.sh
@@ -4,6 +4,6 @@ CURR_DIR=$2;
 cd $1;
 gunzip -c -S .xcactivitylog `ls -t | grep 'xcactivitylog' | head -n1` > output2.log;
 awk '{ gsub("\r", "\n"); print }' output2.log > unixfile.txt;
-LOG=`echo "puts /(^= All tests.+Test Suite 'All tests' finished at .+\.$)/m.match(File.read(\"unixfile.txt\"))" | ruby`;
+LOG=`echo "puts /(^Test Suite '[\w-]+?\.xctest' started at .+?Test Suite '[\w-]+?\.xctest' (failed|passed).+?\.$)/m.match(File.read(\"unixfile.txt\"))" | ruby`;
 cd $CURR_DIR
 echo "$LOG" | xcpretty -t --report flatiron
